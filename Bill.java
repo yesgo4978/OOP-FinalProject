b8b400ab2f6c7f5882acdf38f20ae044553c4837
jeff-pumas01 <34252994+jeff-pumas01@users.noreply.github.com>
2017-12-07 18:10:33 -0600

    Add files via upload

---
+++ Bill.java
@@ -0,0 +1,18 @@
+String billName;
+int billPrice;
+int paidPrice;
+int totalRemaing;
+int percentageOfTotal;
+
+public Bill {
+	String billName = sc.nextString();
+	int billPrice = sc.nextInt();
+	int paidPrice = sc.nextInt();
+	int totalRemaing = billPrice - paidPrice;
+	
+}
+public class Bill {
+	public Static void main(String[] args) {
+		
+	}
+}
